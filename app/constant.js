45ac403be8a1291702fd46359082bc18f6c53230
诸岳 <fuping.dfp@antfin.com>
2019-08-19 15:21:09 +0800

    fix: #10

--- app/constant.js
+++ app/constant.js
@@ -1,45 +1,49 @@
-module.exports = {
-  REPO_LIST: Object.keys(this.REPO_CONFIG),
-  REPO_CONFIG: {
-    g: {
-      reproduceLink: '',
-      labels: [
-        'render',
-        'event',
-        'animation',
-        'group',
-        'shape',
-        'bbox',
-        'document',
-        'svg',
-        'canvas',
-      ],
-    },
-    g2: {
-      reproduceLink: '',
-      labels: [
-        'chart',
-        'axis',
-        'legend',
-        'tooltip',
-        'geom',
-        'guide',
-        'label',
-        'scale',
-        'coord',
-        'event',
-        'animation',
-        'theme',
-        'dataset',
-        'slider',
-        'brush',
-        'svg',
-        'performance',
-      ],
-    },
-    g6: {
-      reproduceLink: '',
-      labels: [],
-    },
+const REPO_CONFIG = {
+  g: {
+    reproduceLink: 'https://codesandbox.io/s/g-reproduction-template-w9gb5',
+    labels: [
+      'render',
+      'event',
+      'animation',
+      'group',
+      'shape',
+      'bbox',
+      'document',
+      'svg',
+      'canvas',
+    ],
   },
+  g2: {
+    reproduceLink: 'https://codesandbox.io/s/g2-reproduction-template-uvzdf',
+    labels: [
+      'chart',
+      'axis',
+      'legend',
+      'tooltip',
+      'geom',
+      'guide',
+      'label',
+      'scale',
+      'coord',
+      'event',
+      'animation',
+      'theme',
+      'dataset',
+      'slider',
+      'brush',
+      'svg',
+      'performance',
+    ],
+  },
+  g6: {
+    reproduceLink: 'https://codesandbox.io/s/g6-reproduction-template-uvzdf',
+    labels: [],
+  },
+};
+
+const REPO_LIST = Object.keys(this.REPO_CONFIG);
+
+module.exports = {
+  REPO_LIST,
+  REPO_CONFIG,
 };
